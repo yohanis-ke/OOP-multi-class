3a6426ff3bc5bab378eb6cdf27d3b9b3d0047b19
yohanis-ke <yohanes.ke@gmail.com>
2018-09-02 19:44:28 -0400

    OOP multi class program submit

--- shopping.rb
+++ shopping.rb
@@ -1,15 +1,49 @@
-require_relative 'product'
+require_relative("product")
+class Cart
+  @@cart = []
 
 
-# bread = Product.new('bread', '1.25', 0)
-# milk = Product.new('meat', 15.0 , 0.13)
+  def self.add(name, base_price, tax_rate = 1.15)
+    new_product = Product.new(name, base_price, tax_rate)
+    @@cart << new_product
+    return new_product
+  end
 
-milk = Product.create('Milk', '4.55', '0.13')
+  def self.show
+    @@cart
+  end
 
-# puts bread.total_befor
-puts milk.show_product.inspect
-puts milk.add_product
-# puts milk.show_product.inspect
-# puts milk.reomove_item
-# puts milk.show_product.inspect
-puts milk.total_befor
+  def self.remove(product)
+    @@cart.delete(product)
+  end
+
+  def self.total_cost
+    sum = 0
+    @@cart.each do |product|
+      sum += product.base_price
+    end
+    return sum
+  end
+
+  def self.total_cost_plus_tax
+    sum = 0
+    @@cart.each do |product|
+      sum += product.total_price
+    end
+    return sum
+  end
+
+end
+
+item1 = Cart.add("Milk", 4.25, 1.15)
+item2 = Cart.add("Tomato", 5.15, 1.15)
+item3 = Cart.add("Avocado", 4.5, 1.15)
+
+puts Cart.show.inspect
+
+Cart.remove(item1)
+
+puts Cart.show.inspect
+
+puts "The total cost is #{Cart.total_cost}"
+puts "The total cost including tax is #{Cart.total_cost_plus_tax}"
